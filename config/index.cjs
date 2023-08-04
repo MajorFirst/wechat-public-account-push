a0c5f8e6558a8848c39f52205de7fb94b64541d6
MajorFirst <1306612848@qq.com>
2023-08-04 13:46:46 +0800

    Update index.cjs
    模板id

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oO0D-6hihE-YVtXeyO_2ZC2qea2I',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'fn8bP8pZY2DwUJa_IFcqOqpAfh4UaJj0uLCNHeXqZMo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-10',
       festivals: [
